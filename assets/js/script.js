53df9a462a8ebc4ae589e68837f6a4a751ae389f
jhonRPdoe <ruangnr@outlook.com>
2022-05-08 23:48:28 -0300

    quarto commit, js e css prontos

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,69 +13,116 @@ function raitingSystem() {
         popin(e);
     });
 
+    window.addEventListener('click', function(e){
+        let containerPopup = document.querySelector('.container-popup')
+        if (!document.querySelector('.container-popup').contains(e.target)){
+            containerPopup.classList.remove('popin');
+        }
+    });
+
     function handleSubmit (e) {
         e.preventDefault();
     } 
 
     function popin (e) {
-        let rate = document.querySelector('.rate');
         let containerPopup = document.querySelector('.container-popup');
-        let btnnum1 = button1.innerHTML;
-        let btnnum2 = button2.innerHTML;
-        let btnnum3 = button3.innerHTML;
-        let btnnum4 = button4.innerHTML;
-        let btnnum5 = button5.innerHTML;
 
         if (!containerPopup.classList.contains('popin')) { 
             containerPopup.classList.add('popin');
-        } else {
-            document.addEventListener('click', e => containerPopup.classList.remove('popin'));
-        }
+        } 
     }
     
     function numSelect () {
         button1.addEventListener('click', e => {
-            !button1.classList.contains('botao-clicado') ? button1.classList.add('botao-clicado') : button1.classList.remove('botao-clicado');
+            if (!button1.classList.contains('botao-clicado')) { button1.classList.add('botao-clicado') }
             button2.classList.remove('botao-clicado');
             button3.classList.remove('botao-clicado');
             button4.classList.remove('botao-clicado');
             button5.classList.remove('botao-clicado');
+
+            let rate = document.querySelector('.rate');
+            rate.innerHTML = `You selected 1 out of 5`;
+
+            try {
+                let submitDisplay = document.querySelector('.display-off');
+                submitDisplay.classList.remove('display-off');
+            } catch (e) {
+                submitDisplay = '';
+            }
         });
 
         button2.addEventListener('click', e => {
-            !button2.classList.contains('botao-clicado') ? button2.classList.add('botao-clicado') : button2.classList.remove('botao-clicado');
+            if (!button2.classList.contains('botao-clicado')) { button2.classList.add('botao-clicado') }
             button3.classList.remove('botao-clicado');
             button4.classList.remove('botao-clicado');
             button5.classList.remove('botao-clicado');
             button1.classList.remove('botao-clicado');
+
+            let rate = document.querySelector('.rate');
+            rate.innerHTML = `You selected 2 out of 5`;
+
+            try {
+                let submitDisplay = document.querySelector('.display-off');
+                submitDisplay.classList.remove('display-off');
+            } catch (e) {
+                submitDisplay = '';
+            }
         });
 
         button3.addEventListener('click', e => {
-            !button3.classList.contains('botao-clicado') ? button3.classList.add('botao-clicado') : button3.classList.remove('botao-clicado');
+            if (!button3.classList.contains('botao-clicado')) { button3.classList.add('botao-clicado') }
             button4.classList.remove('botao-clicado');
             button5.classList.remove('botao-clicado');
             button1.classList.remove('botao-clicado');
             button2.classList.remove('botao-clicado');
+
+            let rate = document.querySelector('.rate');
+            rate.innerHTML = `You selected 3 out of 5`;
+
+            try {
+                let submitDisplay = document.querySelector('.display-off');
+                submitDisplay.classList.remove('display-off');
+            } catch (e) {
+                submitDisplay = '';
+            }
         });
 
         button4.addEventListener('click', e => {
-            !button4.classList.contains('botao-clicado') ? button4.classList.add('botao-clicado') : button4.classList.remove('botao-clicado');
+            if (!button4.classList.contains('botao-clicado')) { button4.classList.add('botao-clicado') }
             button5.classList.remove('botao-clicado');
             button1.classList.remove('botao-clicado');
             button2.classList.remove('botao-clicado');
             button3.classList.remove('botao-clicado');
+
+            let rate = document.querySelector('.rate');
+            rate.innerHTML = `You selected 4 out of 5`;
+
+            try {
+                let submitDisplay = document.querySelector('.display-off');
+                submitDisplay.classList.remove('display-off');
+            } catch (e) {
+                submitDisplay = '';
+            }
         });
 
         button5.addEventListener('click', e => {
-            !button5.classList.contains('botao-clicado') ? button5.classList.add('botao-clicado') : button5.classList.remove('botao-clicado');
+            if (!button5.classList.contains('botao-clicado')) { button5.classList.add('botao-clicado') }
             button1.classList.remove('botao-clicado');
             button2.classList.remove('botao-clicado');
             button3.classList.remove('botao-clicado');
             button4.classList.remove('botao-clicado');
+
+            let rate = document.querySelector('.rate');
+            rate.innerHTML = `You selected 5 out of 5`;
+
+            try {
+                let submitDisplay = document.querySelector('.display-off');
+                submitDisplay.classList.remove('display-off');
+            } catch (e) {
+                submitDisplay = '';
+            }
         });
     }
     
-    console.log(button1.innerHTML);
-
 }
 raitingSystem();
